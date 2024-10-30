08c1bef626d611ec8c9591767897dce4c2beef0d
RafaelPereiraSantos <rafael.pereira@gympass.com>
2024-10-30 17:29:17 -0300

    chore: start to implement function to list changed files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 const segmentFunctionsURL = 'https://api.segmentapis.com/functions';
 const contentType = 'application/json';
@@ -11,7 +12,26 @@ const authToken = () => {
   return token;
 };
 
-const listChangedFunctionsAndSettings = () => {
+const listChangedFunctionsAndSettings = async () => {
+  const sourceBranch = process.env.GITHUB_REF_NAME;
+  const currentBranch = proces.env.GITHUB_BASE_REF;
+
+  let myOutput = '';
+  let myError = '';
+
+  const options = {
+    listeners : {
+      stdout: (data) => { myOutput += data.toString() },
+      stderr: (data) => { myError += data.toString() },
+    },
+  };
+
+  await exec.exec(`git diff -- '*.js' '*.yaml' '*.yml' --name-status ${sourceBranch} ${currentBranch}`, options);
+  await exec.exec(`git diff --name-status HEAD~1 HEAD`, [], options);
+
+  if (!myError) throw new Error('cannot list diff files');
+  if (!myOutput) return [];
+
   return [
     {
       functionPath: '',
@@ -20,11 +40,11 @@ const listChangedFunctionsAndSettings = () => {
   ]
 };
 
-const extractCode = () => {
+const extractCode = (functionPath) => {
 
 };
 
-const prepareSettings = () => {
+const prepareSettings = (settingsPath) => {
 
 };
 
@@ -42,22 +62,25 @@ const buildFunctionPayload = (code, settings) => {
   };
 };
 
-const postRequest = (headers, payload) => {
-
-};
-
 const handleResponse = (response) => {
-  if (!response.status === 200) throw new Error(`function was not updated status code: [${response.status}]`);
+  if (!response.status === 200) throw new Error(`function was not updated. status code: [${response.status}]`);
 };
 
 const updateSegmentFunction = async (token, functionAddress, settingAddress) => {
   const code = extractCode(functionAddress);
   const settings = prepareSettings(settingAddress);
 
+  const method = 'POST';
   const headers = buildFunctionHeaders(token);
-  const payload = buildFunctionPayload(code, settings);
+  const body = buildFunctionPayload(code, settings);
 
-  const response = await postRequest(headers, payload);
+  const options = {
+    method,
+    headers,
+    body
+  };
+
+  const response = await fetch('POST', options);
 
   handleResponse(response);
 };
