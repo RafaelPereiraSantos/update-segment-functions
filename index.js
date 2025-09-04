3ec3a1ba790cdbe6f91a263ca05743c9cd8dcabc
RafaelPereiraSantos <rafael.pereira@gympass.com>
2025-09-04 15:48:31 -0300

    chore: update code to read settings and functions

--- index.js
+++ index.js
@@ -7,17 +7,54 @@ const yaml = require('js-yaml');
 const segmentFunctionsURL = 'https://api.segmentapis.com/functions';
 const contentType = 'application/json';
 
+const defaultConfigFilePath = './segment-functions-config.yaml'
+
 /**
  * This function retrieve the authorization token that will be used to authenticate with Segment.
  *
  * @returns {String} The raw authentication token.
  */
 const authToken = () => {
-  const token = core.getInput('authorization-token');
-  if (!authToken) throw new Error('authorization token can not be blank');
+    const token = core.getInput('authorization-token');
+    if (!authToken) throw new Error('authorization token can not be blank');
 
-  return token;
+    return token;
 };
+
+/**
+ * This function validates a function setting.
+ *
+ * @param {Object} functionsSettings The path of the yaml file to be read and converted.
+ */
+var validateFunctionSettings = (functionsSettings) => {
+    if (!functionsSettings.functionID) throw new Error('missing functionID');
+    if (!functionsSettings.displayName) throw new Error('missing displayName');
+    if (!functionsSettings.description) throw new Error('missing description');
+    if (!functionsSettings.path) throw new Error('missing path');
+
+    if (functionsSettings.settings) {
+        functionsSettings.settings.forEach(setting => {
+        if (!setting.name) throw new Error('settings present but missing name');
+        if (!setting.label) throw new Error('settings present but missing label');
+        if (!setting.description) throw new Error('settings present but missing description');
+        if (!setting.required) throw new Error('settings present but missing required');
+        if (!setting.sensitive) throw new Error('settings present but missing sensitive');
+        if (!setting.type) throw new Error('settings present but missing type');
+        });
+    };
+};
+
+function execPromise(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return reject(stderr);
+        }
+        resolve({ stdout, stderr });
+        });
+    });
+}
 
 /**
  * This functions is responsible for checking which files had changed between the current branch and the one that was
@@ -27,70 +64,47 @@ const authToken = () => {
  * @returns {[Object]} A list of objects containing the pair of javascript and yaml files that must be update into
  * Segment.
  */
-const listChangedFunctionsAndSettings = async () => {
-  const octokit = github.getOctokit(core.getInput('github-token'));
-  const { data: changedFiles } = await octokit.rest.pulls.listFiles({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: core.getInput('pr-number'),
-  });
+const listChangedFunctionsAndSettings = async (filePath) => {
+    core.info('reading configuration file: ', filePath);
 
-  const configFile = file => /.yaml$|.YAML$|.yml$/i.test(file);
-  const codeFile = file => /.js$/i.test(file);
-  const testFile = file => /.test.js/i.test(file);
-  const onlyCodeFile = file => codeFile(file) && !testFile(file);
-  const configOrCodeFile = file => configFile(file) || onlyCodeFile(file);
+    const functionsAndSettingsToUpdate = [];
 
-  const filesOfInterest = [];
-  const pathsOfInterest = [];
+    await execPromise('git fetch origin master');
 
-  for (const changedFile of changedFiles) {
-    const filePath = changedFile.filename;
+    return fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
 
-    if (configOrCodeFile(filePath)){
-      filesOfInterest.push(filePath);
+            throw new Error('no configurations file defined');
+        }
 
-      const partsOfPath = filePath.split('/');
+        const diffCommand = 'git diff --name-only --diff-filter=AM origin/master...HEAD';
+        return execPromise(diffCommand).then(({ stdout }) => {
+        const changedFiles = stdout.split('\n').filter(line => line.trim() !== '');
 
-      partsOfPath.pop();
+        var listOfFunctionsAndSettingsPath = {};
 
-      const fullPath = partsOfPath.join('/');
+        try {
+            listOfFunctionsAndSettingsPath = yaml.load(data);
+        } catch (yamlErr) {
+            throw new Error('error parsing configuration file:', yamlErr);
+        }
 
-      pathsOfInterest.push(fullPath);
-    }
-  }
+        return listOfFunctionsAndSettingsPath.functions.forEach(functionAndSettingsPath => {
+            validateFunctionSettings(functionAndSettingsPath);
 
-  const pathsThatHaveChanges = new Set(pathsOfInterest);
+            const codeChanged = changedFiles.includes(functionAndSettingsPath.codePath);
+            const settingsChanged = changedFiles.includes(functionAndSettingsPath.settingsPath);
 
-  const functionsAndSettingsToUpdate = [];
-
-  pathsThatHaveChanges.forEach(pathThatHaveChanges => {
-    var codeFileName = null;
-    var configFileName = null;
-
-    const defineAsConfigOrCode = file => {
-      if (onlyCodeFile(file)) {
-        codeFileName = file;
-
-        return;
-      }
-
-      if (configFile(file)) {
-        configFileName = file;
-      };
-    }
-
-    fs.readdirSync(pathThatHaveChanges).forEach(defineAsConfigOrCode);
-
-    const buildPath = fileName => pathThatHaveChanges + '/' + fileName;
-
-    functionsAndSettingsToUpdate.push(
-      {
-        functionPath: buildPath(codeFileName),
-        settingPath: buildPath(configFileName),
-      }
-    )
-  });
+            if (codeChanged || settingsChanged) {
+                functionsAndSettingsToUpdate.push({
+                    codePath: functionAndSettingsPath.codePath,
+                    settingPath: functionAndSettingsPath.settingsPath,
+                });
+            }
+        });
+        });
+    });
 
   return functionsAndSettingsToUpdate;
 };
@@ -98,36 +112,10 @@ const listChangedFunctionsAndSettings = async () => {
 /**
  * This function extracts the source code from a function to be sent to Segment.
  *
- * @param {String} functionPath The path of the function.
+ * @param {String} codePath The path of the function.
  * @returns {String} The source code of the function present at the given path.
  */
-const extractCode = (functionPath) => fs.readFileSync(functionPath, 'utf8');
-
-/**
- * This function reads the content of a yaml file and prepare a object with all alike its structure.
- *
- * @param {String} settingsPath The path of the yaml file to be read and converted.
- * @returns {Object} An object containg all configuration info inside the yaml file.
- */
-var prepareSettings = (settingsPath) => {
-  const fileData = fs.readFileSync(settingsPath, 'utf8')
-  const doc = yaml.load(fileData);
-
-  if (!doc.functionID) throw new Error('missing functionID');
-
-  if (doc.settings) {
-    doc.settings.forEach(setting => {
-      if (!setting.name) throw new Error('settings present but missing name')
-      if (!setting.label) throw new Error('settings present but missing label')
-      if (!setting.description) throw new Error('settings present but missing description')
-      if (!setting.required) throw new Error('settings present but missing required')
-      if (!setting.sensitive) throw new Error('settings present but missing sensitive')
-      if (!setting.type) throw new Error('settings present but missing type')
-    });
-  };
-
-  return doc;
-};
+const extractCode = (codePath) => fs.readFileSync(codePath, 'utf8');
 
 /**
  * This function is responsible for generating the URL that will be used to update a function on Segment.
@@ -143,55 +131,55 @@ const buildSegmentPatchURL = (functionID) => `${segmentFunctionsURL}/${functionI
  * @param {Response} response The response from Segment request.
  */
 const handleResponse = async (response) => {
-  const status = response.status;
+    const status = response.status;
 
-  if (status !== 200) {
-    const body = await response.json();
-    let errors = '';
+    if (status !== 200) {
+        const body = await response.json();
+        let errors = '';
 
-    body.errors.forEach(error => {
-      core.info(`error: ${error}`);
-      errors += error.message;
-    });
+        body.errors.forEach(error => {
+        core.info(`error: ${error}`);
+        errors += error.message;
+        });
 
-    throw new Error(`function was not updated, status code: [${status}], errors: [${errors}]`);
-  }
+        throw new Error(`function was not updated, status code: [${status}], errors: [${errors}]`);
+    }
 };
 
 /**
  * This function updates a function on Segment with the new code and configurations.
  *
  * @param {String} token The authentication token that will be used to authenticate with Segment.
- * @param {String} functionPath The path of the function that will be updated.
+ * @param {String} codePath The path of the function that will be updated.
  * @param {String} settingsPath  The path of the yaml file that contains the configurations of the function.
  * @returns {Promise<void>} A promise that resolves when the function was successfully updated on Segment.
  */
-const updateSegmentFunction = async (token, functionPath, settingsPath) => {
-  const code = extractCode(functionPath);
-  const settings = prepareSettings(settingsPath);
+const updateSegmentFunction = async (token, codePath, settingsPath) => {
+    const code = extractCode(codePath);
+    const settings = prepareSettings(settingsPath);
 
-  const method = 'PATCH';
-  const headers = {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': contentType,
-  };
-  const payload = {
-    code,
-    ...settings,
-  };
+    const method = 'PATCH';
+    const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': contentType,
+    };
+    const payload = {
+        code,
+        ...settings,
+    };
 
-  const options = {
-    method,
-    headers,
-    body: JSON.stringify(payload),
-  };
+    const options = {
+        method,
+        headers,
+        body: JSON.stringify(payload),
+    };
 
-  const response = await fetch(
-    buildSegmentPatchURL(settings.functionID),
-    options,
-  );
+    const response = await fetch(
+        buildSegmentPatchURL(settings.functionID),
+        options,
+    );
 
-  handleResponse(response);
+    handleResponse(response);
 };
 
 /**
@@ -199,22 +187,22 @@ const updateSegmentFunction = async (token, functionPath, settingsPath) => {
  * configurations to update them later on Segment with the new code and/or configurations.
  */
 const updateSegmentFunctions = async () => {
-  const token = authToken();
-  const functionsAndSettings = await listChangedFunctionsAndSettings();
+    const token = authToken();
+    const segmentFunctionsConfigPath = core.getInput('segment-functions-config-path');
+    return listChangedFunctionsAndSettings(segmentFunctionsConfigPath || defaultConfigFilePath)
+        .then(functionsAndSettings => {
 
-  for (let i = 0; i < functionsAndSettings.length; i++) {
-    const functionAndSetting = functionsAndSettings[i];
-
-    await updateSegmentFunction(
-      token,
-      functionAndSetting.functionPath,
-      functionAndSetting.settingPath,
-    )
-  };
+        return functionsAndSettings.forEach(functionAndSettings => updateSegmentFunction(
+                token,
+                functionAndSettings.codePath,
+                functionAndSettings.settingsPath,
+            )
+        );
+    });
 };
 
 try {
-  updateSegmentFunctions();
+    updateSegmentFunctions();
 } catch (error) {
-  core.setFailed(error.message);
-};
+    core.setFailed(error.message);
+}
